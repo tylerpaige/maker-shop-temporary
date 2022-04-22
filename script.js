d37013f4c514fd3d31376d6a16b158bf5ae571d5
Tyler Paige <tylerclarkpaige@gmail.com>
2022-04-22 12:29:02 -0400

    Make cool browser chrome color feature

--- script.js
+++ script.js
@@ -1,25 +1,51 @@
 const ALLOWED_COLORS = ["white", "kraft", "chartreuse", "pink"];
+const COLORS = {
+  black: "#000000",
+  blue: "#0048C1",
+  chartreuse: "#A7D75C",
+  cyan: "#00A1E0",
+  "kelly-green": "#008000",
+  kraft: "#FFC000",
+  pink: "#FFA7EB",
+  purple: "#9F1894",
+  red: "#F70000",
+  white: "#ffffff",
+  yellow: "#F7EB61",
+};
 const INITIALIZED_CLASS = "initialized";
 
 const state = {};
 
 const readColorFromLocalStorage = () => {
   return window.localStorage.getItem("maker-shop-color");
-}
+};
 
 const setColorInLocalStorage = (color) => {
   return window.localStorage.setItem("maker-shop-color", color);
-}
+};
 
 const getClassNameFromColor = (color) => {
   return `color-${color}`;
-}
+};
+
+const getBrowserColorFromColor = (color) => {
+  switch (color) {
+    case "white":
+      return COLORS.red;
+    case "kraft":
+      return COLORS.purple;
+    case "pink":
+      return COLORS["kelly-green"];
+    case "chartreuse":
+      return COLORS.blue;
+  }
+};
 
 const update = (update) => {
   const previousState = Object.assign({}, state);
   Object.assign(state, update);
   render(update, previousState);
-}
+};
 
 const updateColor = (color) => {
   if (!ALLOWED_COLORS.includes(color)) {
@@ -28,7 +54,10 @@ const updateColor = (color) => {
 
   update({ color });
   setColorInLocalStorage(color);
-}
+  document
+    .querySelector("meta[name=theme-color]")
+    .setAttribute("content", getBrowserColorFromColor(color));
+};
 
 const render = (update, previousState) => {
   if (update.hasOwnProperty("initialized")) {
@@ -38,7 +67,7 @@ const render = (update, previousState) => {
   if (update.hasOwnProperty("color")) {
     renderColor(previousState);
   }
-}
+};
 
 const renderInitializedState = () => {
   if (state.initialized) {
@@ -46,15 +75,15 @@ const renderInitializedState = () => {
   } else {
     document.body.classList.remove(INITIALIZED_CLASS);
   }
-}
+};
 
 const renderColor = (previousState) => {
   if (previousState.color) {
-    document.body.classList.remove(getClassNameFromColor(previousState.color))
+    document.body.classList.remove(getClassNameFromColor(previousState.color));
   }
 
-  document.body.classList.add(getClassNameFromColor(state.color))
-}
+  document.body.classList.add(getClassNameFromColor(state.color));
+};
 
 const handleFormChange = (e) => {
   const color = e.target.value || "white";
@@ -70,16 +99,16 @@ const init = () => {
   // Initial state
   update({
     initialized: false,
-    color: readColorFromLocalStorage() || "white"
-  })
+    color: readColorFromLocalStorage() || "white",
+  });
 
   // Events
   colorPicker.addEventListener("change", handleFormChange);
 
   // Initial render
-  const shouldBeSelected = [...colorOptions].find(option => (
-    option.value == state.color
-  )) || colorOptions[0];
+  const shouldBeSelected =
+    [...colorOptions].find((option) => option.value == state.color) ||
+    colorOptions[0];
   shouldBeSelected.checked = true;
   update({ initialized: true });
 };
